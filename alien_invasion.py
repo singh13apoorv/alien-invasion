fe24b2cf24ceef894c7a39394c3dc54594ff9db6
Apoorv Singh <singh13.apoorv@gmail.com>
2023-08-10 21:49:45 +0530

    adding background color to window.

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # setting the background color
+        self.bg_color = (120, 131, 75)
+
     def run_game(self):
         """start the main loop for the game.
         """
@@ -24,6 +27,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # redrawing the screen in each iteration of while loop
+            self.screen.fill(self.bg_color)
 
             # make the most recently drawn screen visible
             pygame.display.flip()
